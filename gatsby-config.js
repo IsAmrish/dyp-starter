e9a47298ed6e5f362bc015ff4ead1555748a030a
isamrish <askmaurya48@gmail.com>
2019-12-05 16:05:08 +0530

    Initial commit for dyp-starter
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Document Your Project Starter`,
+    description: `Make your project docs, tutorials, blogs for projects etc using dyp starter`,
+    author: `@isamrish`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
